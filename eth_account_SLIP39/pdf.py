97ed6d8154305c695b840b3c5a86481230da17ec
Perry Kundert <perry@hardconsulting.com>
2021-12-20 16:34:11 -0700

    Begin to add PDF output

---
+++ eth_account_SLIP39/pdf.py
@@ -0,0 +1,23 @@
+from fpdf		import FPDF
+
+from .generate		import create
+
+FILE_DEFAULT			= "ethereum-SLIP-39.pdf"
+
+class PDF( FPDF ):
+    pass
+
+def output(
+    mnemonics: List[List[str]],
+    account,
+    filename		= None,
+):
+    pdf			= PDF(
+        orientation	= 'L',
+        unit		= 'in',
+        format		= (3, 5),
+    )
+    pdf.add_page()
+    pdf.set_font( 'Arial', 'B', 16 )
+    pdf.cell( 2.5, 4.5, "Hello, world!" )
+    pdf.output( filename or FILE_DEFAULT )
